ac5042b4ead41265eee641ed714e68ebc9b2b5ee
ADS 2024.1M <36129382024.1M@senacrjedu.local>
2025-08-12 08:47:34 -0300

    coisa

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
 const novaTarefa = document.querySelector("[data-form-button]");
 
 
-novaTarefa.addEventListener('click', () => {
+novaTarefa.addEventListener('click', (evento) => {
+    evento.preventDefault();
     const tarefa = document.querySelector("[data-form-input]")
     const valor = tarefa.value;
     console.log(valor);
 
 });
 
-//=====================================================================================
+//=====================================================================================//
 // EVENTO
 // Onde vai acontecer
 // Qual evento vai acontecer - click
-// O que vai acontecer - fui clicado - Ação => função  
-
-// function somar(num1, num2){
-//     console.log(num1+num2)
-// }
-
-// // somar(10,10);
-
-// const multiplicar = (num1 , num2) => {
-//     console.log(num1*num2);
-// }
-// multiplicar(10,10);
+// O que vai acontecer - fui clicado - Ação => função
